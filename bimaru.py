26889099c9af88af2519d8d572d9b8bce8e657bd
Henrique Pimentel <henriquerpimentel@gmail.com>
2023-05-24 15:09:55 +0100

    updates and gui

--- bimaru.py
+++ bimaru.py
@@ -2,9 +2,9 @@
 # Devem alterar as classes e funções neste ficheiro de acordo com as instruções do enunciado.
 # Além das funções e classes já definidas, podem acrescentar outras que considerem pertinentes.
 
-# Grupo 00:
-# 00000 Nome1
-# 00000 Nome2
+# Grupo 120:
+# 103468 Pedro Gomes
+# 104156 Henrique Pimentel
 
 import sys
 from search import (
@@ -16,6 +16,8 @@ from search import (
 	greedy_search,
 	recursive_best_first_search,
 )
+
+from gui import Application
 
 
 class BimaruState:
@@ -52,6 +54,9 @@ class Board:
 	def notEmpty(self, row: int, col: int):
 		return self.matrix[row][col] != '.'
 
+	def isPiece(self, row: int, col: int):
+		return self.isValid(row,col) and (self.matrix[row][col] == 'M' or self.matrix[row][col] == 'U' or self.matrix[row][col] == 'T' or self.matrix[row][col] == 'B' or self.matrix[row][col] == 'C' or self.matrix[row][col] == 'L' or self.matrix[row][col] == 'R')
+
 	def get_value(self, row: int, col: int) -> str:
 		"""Devolve o valor na respetiva posição do tabuleiro."""
 		if self.isValid(row, col) and self.notEmpty(row, col):
@@ -70,10 +75,8 @@ class Board:
 		return (self.get_value(row, col-1), self.get_value(row, col+1))
 
 	def get_water_positions(self, row:int, col: int, type: str):
+		"""Retorna as posições da agua para uma determinada peça"""
 		positions = [(row-1, col-1), (row-1, col), (row, col-1), (row+1, col+1), (row+1, col), (row, col+1), (row+1, col-1), (row-1, col+1)]
-		def valid_pos(pos):
-			return self.isValid(pos[0], pos[1])
-		positions = list(filter(valid_pos, positions))
 		if type == 'T':
 			positions.pop(positions.index((row+1, col)))
 		elif type == 'B':
@@ -96,35 +99,31 @@ class Board:
 				positions.pop(positions.index((row, col+1)))
 				positions.pop(positions.index((row+1, col)))
 				positions.pop(positions.index((row-1, col)))
+		elif type == 'U' or type == 'M':
+			positions.pop(positions.index((row, col-1)))
+			positions.pop(positions.index((row, col+1)))
+			positions.pop(positions.index((row+1, col)))
+			positions.pop(positions.index((row-1, col)))
 
+		def valid_pos(pos):
+			return self.isValid(pos[0], pos[1])
+		positions = list(filter(valid_pos, positions))
 		return positions
-
-			
-	def check_if_row_col_completed(self, row: int, col: int):
-		"""Verifica se a linha e a coluna que foi alterada está terminada"""
-		if self.rows[row] == 0: # verifica linha preenchida
-			self.fill_water([[row, i] for i in range(10)])
-		elif self.rows[row] == self.rows_pieces[row]: # preencher espaço em branco com peças
-			pass
-		if self.columns[col] == 0: # verifica coluna preenchida
-			self.fill_water([[i, col] for i in range(10)])
-		elif self.columns[col] == self.columns_pieces[col]: # preencher espaço em branco com peças
-			pass
-		
-
 	
 	def fill_water(self, cells):
 		"""Preenche multiplas celas com água"""
+		preencheu = False # retorna true se preencheu pelo menos 1 cela com água
 		for cell in cells:
 			row, col = cell
-			if self.get_value(row,col) == None:
+			if self.get_value(row,col) == None and self.isValid(row, col):
 				self.matrix[row][col] = 'W'
 				self.rows_pieces[row] -= 1
-				self.columns_pieces[row] -= 1
-				self.check_if_row_col_completed(row, col)
+				self.columns_pieces[col] -= 1
+				preencheu = True
+		return preencheu
 	
-	
-	def place_piece(self, row: int, col: int, type: str):
+	def add_piece(self, row: int, col: int, type: str):
+		"""Adiciona uma peça à cela correspondente"""
 		if type == 'W':
 			self.fill_water([(row, col)])
 		else:
@@ -133,51 +132,96 @@ class Board:
 			self.rows_pieces[row] -= 1
 			self.columns[col] -= 1
 			self.columns_pieces[col] -= 1
-			self.check_if_row_col_completed(row, col)
-			self.fill_water(self.get_water_positions(row, col, type))
-			if type == 'T':
-				if self.adjacent_vertical_values(row,col)[1] == None: # coloca peça abaixo do topo
-					self.place_piece(row+1, col, 'U')
-			elif type == 'B':
-				if self.adjacent_vertical_values(row,col)[0] == None: # coloca peça acima do baixo
-					self.place_piece(row-1, col, 'U')
-			elif type == 'L':
-				if self.adjacent_horizontal_values(row,col)[1] == None: 
-					self.place_piece(row, col+1, 'U')
-			elif type == 'R':
-				if self.adjacent_horizontal_values(row,col)[0] == None: 
-					self.place_piece(row, col-1, 'U')
-			elif type == 'M':
-				horizontal = self.adjacent_horizontal_values(row,col)
-				vertical = self.adjacent_vertical_values(row,col)
-				if horizontal[0] == 'W' or horizontal[1] == 'W' or not self.isValid(row,col-1) or not self.isValid(row,col+1):
-					self.place_piece(row-1, col, 'U')
-					self.place_piece(row+1, col, 'U')
-				if vertical[0] == 'W' or vertical[1] == 'W' or not self.isValid(row-1,col) or not self.isValid(row+1,col):
-					self.place_piece(row, col-1, 'U')
-					self.place_piece(row, col+1, 'U')
-
-
-	def preload_board(self):
-		for i in range(10):
-			if self.rows[i] == 0: # verifica se a linha está completa
-				self.fill_water([[i, j] for j in range(10)])
-		for i in range(10):
-			if self.columns[i] == 0: # verifica se a coluna está completa
-				self.fill_water([[j, i] for j in range(10)])
-			
 
 	
+
+	def load_board(self):
+		"""Carrega o jogo"""
+		alterado = True
+		while alterado:
+			alterado = False
+			# 1- Preenche celas cheias com água
+			for i in range(10):
+				if self.rows[i] == 0 and self.rows_pieces[i] != 0:
+					self.fill_water([(i, j) for j in range(10)])
+					alterado = True
+				if self.columns[i] == 0 and self.columns_pieces[i] != 0:
+					self.fill_water([(j, i) for j in range(10)])
+					alterado = True
+			# 2- Rodeia as peças com água
+			for row in range(10):
+				for col in range(10):
+					if self.isPiece(row,col):
+						if self.fill_water(self.get_water_positions(row,col,self.get_value(row,col))):
+							alterado = True
+			# 3- Adiciona peças temporarias aos TOP, BOTTOM, MIDDLE, LEFT e RIGHT
+			for row in range(10):
+				for col in range(10):
+					value = self.get_value(row,col)
+					horizontal = self.adjacent_horizontal_values(row,col)
+					vertical = self.adjacent_vertical_values(row,col)
+					if (value == 'T' and vertical[1] == None) or (value == 'M' and (horizontal[0] == None or horizontal[1] == None or horizontal[0] == 'W' or horizontal[1] == 'W') and vertical[1] == None):
+						self.add_piece(row+1,col,'U')
+						alterado = True
+					if value == 'B' and vertical[0] == None or (value == 'M' and (horizontal[0] == None or horizontal[1] == None or horizontal[0] == 'W' or horizontal[1] == 'W') and vertical[0] == None):
+						self.add_piece(row-1,col,'U')
+						alterado = True
+					if value == 'L' and horizontal[1] == None or (value == 'M' and (vertical[0] == None or vertical[1] == None or vertical[0] == 'W' or vertical[1] == 'W') and horizontal[1] == None):
+						self.add_piece(row,col+1,'U')
+						alterado = True
+					if value == 'R' and horizontal[0] == None or (value == 'M' and (vertical[0] == None or vertical[1] == None or vertical[0] == 'W' or vertical[1] == 'W') and horizontal[0] == None):
+						self.add_piece(row,col-1,'U')
+						alterado = True
+			# 4 - Adiciona peças temporarias a espaços onde só faltam essas para preencher
+			for i in range(10):
+				if self.rows[i] == self.rows_pieces[i] != 0:
+					for j in range(10):
+						if self.get_value(i,j) == None:
+							self.add_piece(i,j,'U')
+							alterado = True
+				if self.columns[i] == self.columns_pieces[i] != 0:
+					for j in range(10):
+						if self.get_value(j,i) == None:
+							self.add_piece(j,i,'U')
+							alterado = True
+			# 5 - Converte peças temporárias em peças permanentes
+			for row in range(10):
+				for col in range(10):
+					value = self.get_value(row,col)
+					if value == 'U':
+						horizontal = self.adjacent_horizontal_values(row,col)
+						vertical = self.adjacent_vertical_values(row,col)
+						# A peça é TOP sse tem uma peça abaixo, e acima dela tem ou agua ou vazio
+						if (vertical[1] == 'U' or vertical[1] == 'B' or vertical[1] == 'M') and (vertical[0] == 'W' or not self.isValid(row-1,col)):
+							self.matrix[row][col] = 'T'
+							alterado = True
+						# A peça é BOTTOM sse tem uma peça acima, e abaixo dela tem ou agua ou vazio
+						elif (vertical[0] == 'U' or vertical[0] == 'T' or vertical[0] == 'M') and (vertical[1] == 'W' or not self.isValid(row+1,col)):
+							self.matrix[row][col] = 'B'
+							alterado = True
+						# A peça é LEFT sse tem uma peça à direita, e à esquerda dela tem ou agua ou vazio
+						elif (horizontal[1] == 'U' or horizontal[1] == 'R' or horizontal[1] == 'M') and (horizontal[0] == 'W' or not self.isValid(row,col-1)):
+							self.matrix[row][col] = 'L'
+							alterado = True
+						# A peça é RIGHT sse tem uma peça à esquerda, e à direita dela tem ou agua ou vazio
+						elif (horizontal[0] == 'U' or horizontal[0] == 'L' or horizontal[0] == 'M') and (horizontal[1] == 'W' or not self.isValid(row,col+1)):
+							self.matrix[row][col] = 'R'
+							alterado = True
+						# A peça é CENTER sse tem água ou vazio à volta dela
+						elif (vertical[0] == 'W' or not self.isValid(row-1,col)) and (vertical[1] == 'W' or not self.isValid(row+1,col)) and (horizontal[0] == 'W' or not self.isValid(row,col-1)) and (horizontal[1] == 'W' or not self.isValid(row,col+1)):
+							self.matrix[row][col] = 'C'
+							alterado = True
+						# A peça é MIDDLE sse tem peças acima e abaixo OU peças à direita e à esquerda
+						elif (self.isPiece(row+1,col) and self.isPiece(row-1,col)) or (self.isPiece(row,col+1) and self.isPiece(row,col-1)):
+							self.matrix[row][col] = 'M'
+							alterado = True
+				
 
 	
 	def print_board(self):
 		"""Imprime o tabuleiro"""
-		i = 0
-		for line in self.matrix:
-			print(' '.join(line) + ' ' + str(self.rows[i]))
-			i += 1
-		list_columns = [str(num) for num in self.columns]
-		print(' '.join(list_columns))
+		app = Application(self.matrix, self.rows, self.columns)
+		app.mainloop()
 
 
 	@staticmethod
@@ -188,15 +232,15 @@ class Board:
 		board.rows = [int(i) for i in sys.stdin.readline().split()[1:]]
 		board.columns = [int(i) for i in sys.stdin.readline().split()[1:]]
 		hints = int(sys.stdin.readline())
-		board.preload_board()
 		for i in range(hints):
 			hint = sys.stdin.readline().split()[1:]
 			row = int(hint[0])
 			col = int(hint[1])
 
 			val = hint[2]
-			board.place_piece(row,col,val)
-			
+			board.add_piece(row,col,val)
+		board.load_board()
+
 
 		
 		# faz o pre-processamento inicial
